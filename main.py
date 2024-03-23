38e830e35d6729f993d31a9a19db1e0881fc023b
igorbasko01 <igorbasko@gmail.com>
2024-03-23 09:23:03 +0200

    add type hinting to remove warningns

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from events import SignInStartedEvent, OneTimePasswordSentEvent, SecurityChallengePassedEvent
+from typing import Type, List, Set
+
+from events import SignInStartedEvent, OneTimePasswordSentEvent, SecurityChallengePassedEvent, Event
 from events_generator import RandomEventsGenerator
 from file_writer import EventsFileWriter
 
 if __name__ == '__main__':
-    events = [
+    events: Set[Type[Event]] = {
         SignInStartedEvent, OneTimePasswordSentEvent, SecurityChallengePassedEvent
-    ]
+    }
     generator = RandomEventsGenerator(events, 'uid', 5)
     with open('events.json', 'w') as file:
         EventsFileWriter.write(generator, file)
